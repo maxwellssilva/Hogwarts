dbe928e10bd216ecef9f08d5fe513733ba9e1bc7
Maxwell Silva <maxwell.ssilvva@gmail.com>
2025-01-22 10:29:27 -0400

    Criar cardCell para tableView

---
+++ Hogwarts/CardCell.swift
@@ -0,0 +1,56 @@
+//
+//  CardCell.swift
+//  Hogwarts
+//
+//  Created by Maxwell Silva on 22/01/25.
+//
+
+import UIKit
+
+class CardCell: UITableViewCell {
+    
+    lazy var personImage: UIImageView = {
+        let image = UIImageView()
+        image.layer.cornerRadius = 8
+        image.clipsToBounds = true
+        image.backgroundColor = .systemYellow
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    lazy var personName: UILabel = {
+        let name = UILabel()
+        name.numberOfLines = 0
+        name.adjustsFontSizeToFitWidth = true
+        name.translatesAutoresizingMaskIntoConstraints = false
+        return name
+    }()
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            setupCardCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCardCell() {
+        addSubview(personImage)
+        addSubview(personName)
+        NSLayoutConstraint.activate([
+            personImage.centerYAnchor.constraint(equalTo: centerYAnchor),
+            personImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            personImage.heightAnchor.constraint(equalToConstant: 80),
+            personImage.widthAnchor.constraint(equalTo: personImage.heightAnchor),
+            
+            personName.centerYAnchor.constraint(equalTo: centerYAnchor),
+            personName.leadingAnchor.constraint(equalTo: personImage.leadingAnchor, constant: 15),
+            personName.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+}
---
+++ Hogwarts/Controller/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Hogwarts
+//
+//  Created by Maxwell da Silva e Silva on 30/01/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    //MARK: Componentes
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Pesquise um personagem"
+        searchBar.searchBarStyle = .default
+        searchBar.tintColor = .systemBlue
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    private lazy var listPerson: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .cyan
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupHomeScreen()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.title = "Hogwarts"
+    }
+
+    //MARK: Métodos
+    func setupHomeScreen() {
+        view.addSubview(searchBar)
+        view.addSubview(listPerson)
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            listPerson.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            listPerson.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            listPerson.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            listPerson.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return charactes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
